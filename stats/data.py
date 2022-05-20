9df1e7ce386e1c8edd6bf9f25f90195c0d4cc3c4
Bart Fiten <bartfiten@hotmail.com>
2022-05-20 15:36:59 +0200

    Update data.py

--- stats/data.py
+++ stats/data.py
@@ -19,7 +19,7 @@ identifiers = identifiers.fillna(method='ffill')
 identifiers.columns = ['game_id', 'year']
 
 games = pd.concat([games, identifiers], axis=1, sort=False)
-games.fillna(' ', inplace=True)
+games = games.fillna(' ')
 
 games.loc[:, 'type'] = pd.Categorical(games.loc[:, 'type'])
 print(games.head())
